715534071bca9e5150f473312328eb7c242c4b64
Giulio Petek <gi-lo@touch-mania.com>
2013-04-27 05:37:02 +0200

    #5 optimizes header view

---
+++ Giulio Petek/Giulio Petek/View/GPSocialCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GPSocialCollectionViewCell.h
+//  Giulio Petek
+//
+//  Created by Giulio Petek on 27.04.13.
+//  Copyright (c) 2013 grandcentrix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPSocialCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Giulio Petek/Giulio Petek/View/GPSocialCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  GPSocialCollectionViewCell.m
+//  Giulio Petek
+//
+//  Created by Giulio Petek on 27.04.13.
+//  Copyright (c) 2013 grandcentrix. All rights reserved.
+//
+
+#import "GPSocialCollectionViewCell.h"
+
+@implementation GPSocialCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
